4a2ff02f7352aa83016a44a35b72212409404cb3
JasonYSQ <1004005788@qq.com>
2018-08-31 11:18:06 +0800

    TestToSpeech

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    public Button btnA;
+    public Button btnB;
+    
+    IEnumerator ChangeScene(string sceneName)
+    {
+        AsyncOperation aysync = Application.LoadLevelAsync(sceneName);
+        yield return aysync;
+    }
+    public void ClickBtnA()
+    {
+        StartCoroutine(ChangeScene("BaiduVoice"));
+    }
+    public void ClickBtnB()
+    {
+        StartCoroutine(ChangeScene("TextToSpeech"));
+    }
+}
